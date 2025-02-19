e13761a49a1dcf27ed06e00594349ddead1693e5
Akasha Karam <2024_akasha.karam@iticbcn.cat>
2025-02-19 19:49:15 +0100

    connect.py acabat

--- database/connect.py
+++ database/connect.py
@@ -1,1 +1,12 @@
-c
+import psycopg2
+
+def connection_db():
+    conn = psycopg2.connect(
+        databases="the_bear",
+        password="admin",
+        user="admin",
+        host="localhost",
+        port="5432"
+    )
+
+    return conn
