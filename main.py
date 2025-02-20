134ba4a9111539e8f78e18d683457be130a16ec3
alvarohc1 <alvaroisaias2002@gmail.com>
2025-02-20 11:57:21 +0100

    maiin.py acabat✔

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import List
 from fastapi import FastAPI
 from services import read
 
-##Otravez
+#OtravezA
 app = FastAPI()
 @app.get("/root", response_model=List[dict])
 async  def read_root():
