ed11687b58001c597d95201b163054a9b9855fd7
Carlos Pérez <2024_carlos.perez@iticbcn.cat>
2025-03-13 21:43:20 +0100

    Todo lo de clase

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 from typing import List
-from fastapi import FastAPI
-from services import read
+from fastapi import FastAPI, Depends
+from services import read, user
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+SQLModel.metadata.create_all(engine)
+
+# Hace lo que hacia le cursor
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/root", response_model=List[dict])
 async def read_root():
     result = read.registre()
     return result
 
+#Para leer los usuarios
+@app.get("/read_users/", response_model= list[dict])
+async def read_user(db:Session = Depends(get_db)):
+    result = user.get_all_users(db)
+    return result
+
+#Para añadir usuarios
+@app.post("/add_users/", response_model=dict)
+async def create_user(name: str, email:str, db:Session = Depends(get_db)):
+    result = user.add_new_user(name, email, db)
+    return result
